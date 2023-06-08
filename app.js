46fa33759241ca05a9bfefb402df4fe4012488e7
Aarya Samatkar <pritisamatkar@gmail.com>
2023-06-08 09:42:29 +0530

    igniniting app

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const parent = React.createElement( "h1",{id:"parent"},[
-    React.createElement("div",{ id:"child1"},[
-        React.createElement("h1",{},"I am h1 tag"),
+    React.createElement("div",{ id:"child1 "},[
+        React.createElement("h1",{},"I am h1 tag hello"),
         React.createElement("h2",{},"I am h2 tag"),
     ]),
     React.createElement("div",{ id:"child2"},[
