f20da183c26ec7aa7d7fe9b9b1afb903264d6da2
Aarya Samatkar <pritisamatkar@gmail.com>
2023-06-08 13:11:34 +0530

    react fragment emoty tag

--- app.js
+++ app.js
@@ -13,14 +13,14 @@ const HeadingComponent1 = () => {
 //this is also React functional component 
 const HeadingComponent2 = () => (
     //react fragment (root)= behaves like an empty tag, not gets added to dom
-    <React.Fragment>
+    <>
         <div>
             <h1 className='functional'> hello from functional component</h1>
         </div>
         <div>
             <h1 className='functional'> hello from functional component</h1>
         </div>
-    </React.Fragment>
+    </>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
