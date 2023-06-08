04b018b7330c3336a860ebfc59d128f9cfd11b84
Aarya Samatkar <pritisamatkar@gmail.com>
2023-06-08 11:13:11 +0530

    react functional component

--- app.js
+++ app.js
@@ -4,5 +4,14 @@ import ReactDOM from 'react-dom/client';
 //jsx
 const heading = (<h1 className='heading'> Hello world</h1>);
 
+//standard React functional component
+
+const HeadingComponent1 = () => {
+    return <h1>Hello from functional component!!</h1>;
+}
+
+//this is also React functional component 
+const HeadingComponent2 = () => <h1 className='functional'> hello from functional component</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
