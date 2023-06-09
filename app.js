6fa3d60472e3eb2f00ef3bacfb5820b6cdde0911
Aarya Samatkar <pritisamatkar@gmail.com>
2023-06-09 12:03:55 +0530

    mapping over dynamic data

--- app.js
+++ app.js
@@ -5,6 +5,535 @@ import ReactDOM from 'react-dom/client';
 const styleCard = {
     backgroundColor: "#f0f0f0",
 };
+const restList = [
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "18772",
+        "name": "California Burrito",
+        "uuid": "2c4121b2-a569-429f-b5f2-aac00f7458c2",
+        "city": "1",
+        "area": "Marathahalli",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "zwwvf8n0jlo1bjx19qzg",
+        "cuisines": [
+          "Mexican",
+          "American",
+          "Salads",
+          "Continental",
+          "Keto",
+          "Healthy Food"
+        ],
+        "tags": [],
+        "costForTwo": 25000,
+        "costForTwoString": "₹250 FOR TWO",
+        "deliveryTime": 25,
+        "minDeliveryTime": 25,
+        "maxDeliveryTime": 25,
+        "slaString": "25 MINS",
+        "lastMileTravel": 3,
+        "slugs": {
+          "restaurant": "california-burrito-marathahalli-village-marathahalli",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "132, Yemalur Main Road, Marathahalli Village, Marathahalli, Bengaluru, Karnataka - 560037",
+        "locality": "Marathahalli",
+        "parentId": 1252,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3800,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3800,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3800",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=7060399~p=19~eid=00000188-9eb0-3f99-0a15-24ff00a31301",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "3 kms",
+        "hasSurge": false,
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50",
+          "discountTag": "",
+          "headerTypeV2": 0
+        },
+        "sla": {
+          "restaurantId": "18772",
+          "deliveryTime": 25,
+          "minDeliveryTime": 25,
+          "maxDeliveryTime": 25,
+          "lastMileTravel": 3,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "4.4",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "424658",
+        "name": "Asha Tiffins",
+        "uuid": "d79cd5d9-7158-45f1-a198-e563f0e46406",
+        "city": "1",
+        "area": "Aecs Layout",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "hpncnz3sfv3gigsukkts",
+        "cuisines": [
+          "South Indian"
+        ],
+        "tags": [],
+        "costForTwo": 20000,
+        "costForTwoString": "₹200 FOR TWO",
+        "deliveryTime": 25,
+        "minDeliveryTime": 25,
+        "maxDeliveryTime": 25,
+        "slaString": "25 MINS",
+        "lastMileTravel": 3.799999952316284,
+        "slugs": {
+          "restaurant": "asha-tiffins-marathahalli-marathahalli",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "#551,AECS Layout main road, 'B' Block AECS Layout,Opp.BBMP ward office,Marathahalli,Bangalore - 560037",
+        "locality": "B Block",
+        "parentId": 236243,
+        "unserviceable": false,
+        "veg": true,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3800,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3800,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3800",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "3.7 kms",
+        "hasSurge": false,
+        "aggregatedDiscountInfoV3": {
+          "header": "30% OFF",
+          "subHeader": "UPTO ₹70",
+          "discountTag": "",
+          "headerTypeV2": 0
+        },
+        "sla": {
+          "restaurantId": "424658",
+          "deliveryTime": 25,
+          "minDeliveryTime": 25,
+          "maxDeliveryTime": 25,
+          "lastMileTravel": 3.799999952316284,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "4.4",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+        "type": "restaurant",
+        "data": {
+          "type": "F",
+          "id": "424658",
+          "name": "Aarya foods",
+          "uuid": "d79cd5d9-7158-45f1-a198-e563f0e46406",
+          "city": "1",
+          "area": "Aecs Layout",
+          "totalRatingsString": "10000+ ratings",
+          "cloudinaryImageId": "qhiy6coolxjequzdstn0",
+          "cuisines": [
+            "South Indian"
+          ],
+          "tags": [],
+          "costForTwo": 30000,
+          "costForTwoString": "₹200 FOR TWO",
+          "deliveryTime": 29,
+          "minDeliveryTime": 25,
+          "maxDeliveryTime": 25,
+          "slaString": "25 MINS",
+          "lastMileTravel": 3.799999952316284,
+          "slugs": {
+            "restaurant": "asha-tiffins-marathahalli-marathahalli",
+            "city": "bangalore"
+          },
+          "cityState": "1",
+          "address": "#551,AECS Layout main road, 'B' Block AECS Layout,Opp.BBMP ward office,Marathahalli,Bangalore - 560037",
+          "locality": "B Block",
+          "parentId": 236243,
+          "unserviceable": false,
+          "veg": true,
+          "select": false,
+          "favorite": false,
+          "tradeCampaignHeaders": [],
+          "chain": [],
+          "feeDetails": {
+            "fees": [
+              {
+                "name": "distance",
+                "fee": 3800,
+                "message": ""
+              },
+              {
+                "name": "time",
+                "fee": 0,
+                "message": ""
+              },
+              {
+                "name": "special",
+                "fee": 0,
+                "message": ""
+              }
+            ],
+            "totalFees": 3800,
+            "message": "",
+            "title": "Delivery Charge",
+            "amount": "3800",
+            "icon": ""
+          },
+          "availability": {
+            "opened": true,
+            "nextOpenMessage": "",
+            "nextCloseMessage": ""
+          },
+          "longDistanceEnabled": 0,
+          "rainMode": "NONE",
+          "thirdPartyAddress": false,
+          "thirdPartyVendor": "",
+          "adTrackingID": "",
+          "badges": {
+            "imageBased": [],
+            "textBased": [],
+            "textExtendedBadges": []
+          },
+          "lastMileTravelString": "3.7 kms",
+          "hasSurge": false,
+          "aggregatedDiscountInfoV3": {
+            "header": "30% OFF",
+            "subHeader": "UPTO ₹70",
+            "discountTag": "",
+            "headerTypeV2": 0
+          },
+          "sla": {
+            "restaurantId": "424658",
+            "deliveryTime": 25,
+            "minDeliveryTime": 25,
+            "maxDeliveryTime": 25,
+            "lastMileTravel": 3.799999952316284,
+            "lastMileDistance": 0,
+            "serviceability": "SERVICEABLE",
+            "rainMode": "NONE",
+            "longDistance": "NOT_LONG_DISTANCE",
+            "preferentialService": false,
+            "iconType": "EMPTY"
+          },
+          "promoted": false,
+          "avgRating": "4.5",
+          "totalRatings": 10000,
+          "new": false
+        },
+        "subtype": "basic"
+      },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "636313",
+        "name": "The Rameshwaram Cafe",
+        "uuid": "ee7e2a22-42c6-4f59-b0a1-d1df6faf403d",
+        "city": "1",
+        "area": "Brookefield",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "0adee855c65fa947a256764a87a55658",
+        "cuisines": [
+          "South Indian"
+        ],
+        "tags": [],
+        "costForTwo": 25000,
+        "costForTwoString": "₹250 FOR TWO",
+        "deliveryTime": 23,
+        "minDeliveryTime": 23,
+        "maxDeliveryTime": 23,
+        "slaString": "23 MINS",
+        "lastMileTravel": 3.9000000953674316,
+        "slugs": {
+          "restaurant": "the-rameshwaram-café-whitefield-whitefield",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "AIKYAM - THE RAMESHWARAM CAFE PLOT NO. 01, ITPL MAIN ROAD, GREEN AVENUE - BENGALURU, INDIA, B.B.M.P East, Karnataka-560037",
+        "locality": "ITPL Main road",
+        "parentId": 384316,
+        "unserviceable": false,
+        "veg": true,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3800,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3800,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3800",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "3.9 kms",
+        "hasSurge": false,
+        "sla": {
+          "restaurantId": "636313",
+          "deliveryTime": 23,
+          "minDeliveryTime": 23,
+          "maxDeliveryTime": 23,
+          "lastMileTravel": 3.9000000953674316,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": false,
+        "avgRating": "4.6",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    },
+    {
+      "type": "restaurant",
+      "data": {
+        "type": "F",
+        "id": "262167",
+        "name": "Hotel Empire",
+        "uuid": "c087d70f-5cff-49ec-8b8a-e8b1a07a1977",
+        "city": "1",
+        "area": "Brookefields",
+        "totalRatingsString": "10000+ ratings",
+        "cloudinaryImageId": "weuqdjwzk3azoijehnel",
+        "cuisines": [
+          "North Indian",
+          "Kebabs",
+          "Biryani"
+        ],
+        "tags": [],
+        "costForTwo": 40000,
+        "costForTwoString": "₹400 FOR TWO",
+        "deliveryTime": 39,
+        "minDeliveryTime": 39,
+        "maxDeliveryTime": 39,
+        "slaString": "39 MINS",
+        "lastMileTravel": 3,
+        "slugs": {
+          "restaurant": "hotel-empire-marathahalli-marathahalli",
+          "city": "bangalore"
+        },
+        "cityState": "1",
+        "address": "EMPIRE RESTAURANT ASK TOWERS,NO 7/3 OF THOOBARAHALLI VILLAGE,VARTHUR VILLAGE,VARTHOOR HOBLI 560037",
+        "locality": "",
+        "parentId": 475,
+        "unserviceable": false,
+        "veg": false,
+        "select": false,
+        "favorite": false,
+        "tradeCampaignHeaders": [],
+        "ribbon": [
+          {
+            "type": "PROMOTED"
+          }
+        ],
+        "chain": [],
+        "feeDetails": {
+          "fees": [
+            {
+              "name": "distance",
+              "fee": 3800,
+              "message": ""
+            },
+            {
+              "name": "time",
+              "fee": 0,
+              "message": ""
+            },
+            {
+              "name": "special",
+              "fee": 0,
+              "message": ""
+            }
+          ],
+          "totalFees": 3800,
+          "message": "",
+          "title": "Delivery Charge",
+          "amount": "3800",
+          "icon": ""
+        },
+        "availability": {
+          "opened": true,
+          "nextOpenMessage": "",
+          "nextCloseMessage": ""
+        },
+        "longDistanceEnabled": 0,
+        "rainMode": "NONE",
+        "thirdPartyAddress": false,
+        "thirdPartyVendor": "",
+        "adTrackingID": "cid=7028090~p=4~eid=00000188-9eb0-3f99-0a15-24fa00a30476",
+        "badges": {
+          "imageBased": [],
+          "textBased": [],
+          "textExtendedBadges": []
+        },
+        "lastMileTravelString": "3 kms",
+        "hasSurge": false,
+        "aggregatedDiscountInfoV3": {
+          "header": "10% OFF",
+          "subHeader": "UPTO ₹40",
+          "discountTag": "",
+          "headerTypeV2": 0
+        },
+        "sla": {
+          "restaurantId": "262167",
+          "deliveryTime": 39,
+          "minDeliveryTime": 39,
+          "maxDeliveryTime": 39,
+          "lastMileTravel": 3,
+          "lastMileDistance": 0,
+          "serviceability": "SERVICEABLE",
+          "rainMode": "NONE",
+          "longDistance": "NOT_LONG_DISTANCE",
+          "preferentialService": false,
+          "iconType": "EMPTY"
+        },
+        "promoted": true,
+        "avgRating": "3.9",
+        "totalRatings": 10000,
+        "new": false
+      },
+      "subtype": "basic"
+    }
+    
+]
 function Header() {
     return (
         <div className='header'>
@@ -22,39 +551,41 @@ function Header() {
         </div>
     );
 }
-
+const RestaurantCard = (props) =>{
+    //destructuring props
+    const { restData} = props;
+   const { cloudinaryImageId,name, cuisines,costForTwo,deliveryTime,avgRating} = restData?.data;
+   
+    console.log(restData);
+    return ( 
+        <div className='rest-card' style={styleCard}>
+            <img  alt ="rest-logo"className='rest-logo' src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId }/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(", ")}</h4>
+            <h4>{costForTwo/100}</h4>
+            <h4>{avgRating}</h4>
+            <h4>{deliveryTime + "minutes"}</h4>
+        </div>
+    )
+}
 const Body = () =>{
     return (
         <div className='body'>
             <div className='search'> Search</div>
             <div className='restContainer'>
-             < RestaurantCard 
-             resName = "Meghana Foods"
-             cuisins = "Biryani, North-indian"
-             />
-             < RestaurantCard 
-             resName = "KFC Foods"
-             cuisins = "Chiken fast-food"
-             />
+            {
+                restList.map( restaurant => <RestaurantCard
+                restData ={restaurant}
+                />)
+            }
+           
             
             </div>
         </div>
     )
 }
-const RestaurantCard = (props) =>{
-    //destructuring props
-    const { resName, cuisins} = props;
-    console.log(props);
-    return ( 
-        <div className='rest-card' style={styleCard}>
-            <img  alt ="rest-logo"className='rest-logo' src='https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/yfyo8aklppbwdplv7ofp' />
-            <h3>{resName}</h3>
-            <h4>{cuisins}</h4>
-            <h4>4.5 stars</h4>
-            <h4>35 mins</h4>
-        </div>
-    )
-}
+
+
 const AppLayout = () =>{
     return (
     <div className='app'>
